e62492627f769ff9df79dbc933c4f0150e826165
Felipe Lohan <felipelohan03052006@gmail.com>
2025-03-24 15:19:13 -0300

    Created ProductController

--- src/main/java/com/felipelohan/productapi/controller/ProductController.java
+++ src/main/java/com/felipelohan/productapi/controller/ProductController.java
@@ -1,5 +1,18 @@
 package com.felipelohan.productapi.controller;
 
+import com.felipelohan.productapi.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @PostMapping
+    public void save(Product prod){
+        System.out.println("Saved new Product: " + prod.toString());
+    }
 
 }
