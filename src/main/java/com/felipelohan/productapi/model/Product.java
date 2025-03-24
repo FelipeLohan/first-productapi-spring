01861f1f0b8f03c3ed860ac800490dd6c8f539a2
Felipe Lohan <felipelohan03052006@gmail.com>
2025-03-24 15:15:40 -0300

    Added Getters and Setters for Product Model

--- src/main/java/com/felipelohan/productapi/model/Product.java
+++ src/main/java/com/felipelohan/productapi/model/Product.java
@@ -6,10 +6,35 @@ public class Product {
     private String description;
     private double price;
 
-    public Product(String id, String name, String description, double price) {
-        this.id = id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
