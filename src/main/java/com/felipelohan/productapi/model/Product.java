5fa3afc33dbbf91c1fcd59628bc454c61df6aeb5
Felipe Lohan <felipelohan03052006@gmail.com>
2025-03-24 16:10:11 -0300

    Mapped Product Entity for database

--- src/main/java/com/felipelohan/productapi/model/Product.java
+++ src/main/java/com/felipelohan/productapi/model/Product.java
@@ -1,9 +1,25 @@
 package com.felipelohan.productapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private double price;
 
     public String getName() {
