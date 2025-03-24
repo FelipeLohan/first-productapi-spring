fe9f0eb3dc87b0dcf6d077948c4233d0df54c061
Felipe Lohan <felipelohan03052006@gmail.com>
2025-03-24 15:12:11 -0300

    Added Product Model

---
+++ src/main/java/com/felipelohan/productapi/controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.felipelohan.productapi.controller;
+
+public class ProductController {
+
+}
---
+++ src/main/java/com/felipelohan/productapi/model/Product.java
@@ -0,0 +1,15 @@
+package com.felipelohan.productapi.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
